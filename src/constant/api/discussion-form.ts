8149991b77e0c6605aeef42dfb23e6d733c661b1
azarnuzy <azarnuzy@gmail.com>
2023-11-14 17:12:53 +0700

    add: endpoint constant

---
+++ src/constant/api/discussion-form.ts
@@ -0,0 +1,10 @@
+export const DISCUSSION = '/v1/discussion';
+export const DISCUSSION_FORUM = '/v1/discussion/forum';
+export const DISCUSSION_COMMENT = '/v1/discussion/comment';
+export const DISCUSSION_REPLY = '/v1/discussion/reply';
+export const DISCUSSION_LIKE = '/v1/discussion/like';
+export const DISCUSSION_LIKE_COMMENT = '/v1/discussion/like/comment';
+export const DISCUSSION_LIKE_REPLY = '/v1/discussion/like/reply';
+export const DISCUSSION_FORUM_REPORT = '/v1/discussion/forum/report';
+export const DISCUSSION_COMMENT_REPORT = '/v1/discussion/comment/report';
+export const DISCUSSION_REPLY_REPORT = '/v1/discussion/reply/report';
