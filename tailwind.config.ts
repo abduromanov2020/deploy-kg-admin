f360096c8b35d26269221efc931300c1c8d362c7
azarnuzy <azarnuzy@gmail.com>
2023-11-15 09:07:52 +0700

    add: dependencies nextui recoil react query

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,10 @@ import type { Config } from 'tailwindcss';
 import defaultTheme from 'tailwindcss/defaultTheme';
 
 export default {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {
       fontFamily: {
