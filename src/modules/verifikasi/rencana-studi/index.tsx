e0e8ab3ebd55b5dbfbc56a4d101818284a619158
dinda-ayu-syafitri <dindasyafitri@gmail.com>
2023-11-15 17:32:47 +0700

    fix: updated code

--- src/modules/verifikasi/rencana-studi/index.tsx
+++ src/modules/verifikasi/rencana-studi/index.tsx
@@ -1,11 +1,25 @@
-import { Button } from '@nextui-org/react';
+import { Input } from '@nextui-org/react';
 import React from 'react';
+import { IoSearchOutline } from 'react-icons/io5';
 
 export const VerifikasiRencanaStudiModule = () => {
   return (
-    <div>
+    <div className='bg-dark-100 rounded-md'>
+      <div className='border-b border-dark-200 p-4'>
+        <span className='font-semibold '>Verifikasi Rencana Studi</span>
+      </div>
       <div>
-        <Button className='bg-red-600'>Test</Button>
+        <div>
+          <Input
+            type='email'
+            label='Email'
+            placeholder='you@example.com'
+            labelPlacement='outside'
+            startContent={
+              <IoSearchOutline className='text-2xl text-default-400 pointer-events-none flex-shrink-0' />
+            }
+          />
+        </div>
       </div>
     </div>
   );
