d5533367436816520721035851eb0e551dd4754a
azkinf16 <azkifajri16@gmail.com>
2023-11-15 16:40:03 +0700

    improvement: add page & module verifikasi administrasi

---
+++ src/modules/verifikasi/administrasi/module.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const VerifikasiAdministrasiModule = () => {
+  return (
+    <div className='bg-white w-full rounded-md p-3'>
+      <h3>Verifikasi Administrasi</h3>
+
+
+
+    </div>
+  )
+}
+
+export default VerifikasiAdministrasiModule
