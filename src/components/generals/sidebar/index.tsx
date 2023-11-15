9b554f9d0d74c51cc00f2382afb4e16de05548b1
azarnuzy <azarnuzy@gmail.com>
2023-11-15 09:53:06 +0700

    add: some svg icon

--- src/components/generals/sidebar/index.tsx
+++ src/components/generals/sidebar/index.tsx
@@ -1,7 +1,11 @@
+import { Button } from '@nextui-org/react';
 import React, { FC } from 'react';
 
 import Logo from '~/svg/Logo.svg';
 const Sidebar: FC = () => {
+  const defaultContent =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.';
+
   return (
     <div className='w-[280px] min-h-screen py-10 bg-sidebar'>
       <div className='pl-[30px] pb-[30px]'>
@@ -9,8 +13,10 @@ const Sidebar: FC = () => {
       </div>
       <div className='flex gap-6 flex-col'>
         <div className='flex flex-col gap-[10px]'>
-          <p className='text-sm font-medium'>UTAMA</p>
-          <div className='flex gap-[10px]'></div>
+          <p className='text-sm font-medium text-white'>UTAMA</p>
+          <div className='flex gap-[10px]'>
+            <Button color='primary'>Button</Button>
+          </div>
         </div>
       </div>
     </div>
