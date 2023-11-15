f8a50b680e85c23c71e20b56ec133fb8edb1bd59
Zul Ham Fahlevi <zulham.fahlevi.73@gmail.com>
2023-11-15 16:51:16 +0700

    add tailwind config next ui

---
+++ src/modules/sekilas-ilmu/index.tsx
@@ -0,0 +1,22 @@
+import { Button } from '@nextui-org/react';
+import React from 'react';
+
+const SekilasIlmuModule = () => {
+  return (
+    <main className='flex flex-col gap-6'>
+      <div className='bg-white'>
+        <Button className='bg-primary-500'>Button</Button>
+        {/* <Breadcrumbs>
+          <BreadcrumbItem>Home</BreadcrumbItem>
+          <BreadcrumbItem>Music</BreadcrumbItem>
+          <BreadcrumbItem>Artist</BreadcrumbItem>
+          <BreadcrumbItem>Album</BreadcrumbItem>
+          <BreadcrumbItem>Song</BreadcrumbItem>
+        </Breadcrumbs> */}
+      </div>
+      <div className='bg-white'>2</div>
+    </main>
+  );
+};
+
+export default SekilasIlmuModule;
