93e583dc5f0676a32f44897915d77bdd33aaa743
Naza Zulfiqi <zulfiqinaza@gmail.com>
2023-11-15 17:47:25 +0700

    improvement: add layout module

--- src/modules/rencana-studi/index.tsx
+++ src/modules/rencana-studi/index.tsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import {Button} from "@nextui-org/react";
+import React from 'react';
+import { Button, Divider } from '@nextui-org/react';
 
 export const RencanaStudiModule = () => {
   return (
-    <div className='bg-background '>
-      <h1>Ini Rencana Studi Module</h1>
-      <Button className='text-3l'>
-      Button
-    </Button>
-    
-  </div>
-  )
-}
+    <div className='bg-white rounded'>
+      <div className='py-5 px-4'>
+        <p className='text-base font-semibold'>Rencana Studi</p>
+      </div>
+      <Divider className='' />
+      <div className='p-8'>TEST</div>
+      <Button color='primary'>TEst</Button>
+    </div>
+  );
+};
