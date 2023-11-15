97ddeecd2e297c4c53b95b474520fea38fbbe4c5
azarnuzy <azarnuzy@gmail.com>
2023-11-15 09:39:53 +0700

    add: nextui-org in tailwind config

--- src/components/generals/sidebar/index.tsx
+++ src/components/generals/sidebar/index.tsx
@@ -3,9 +3,15 @@ import React, { FC } from 'react';
 import Logo from '~/svg/Logo.svg';
 const Sidebar: FC = () => {
   return (
-    <div className='w-[280px] min-h-screen py-10 bg-gray-800'>
+    <div className='w-[280px] min-h-screen py-10 bg-sidebar'>
       <div className='pl-[30px] pb-[30px]'>
         <Logo className='w-[96px] h-full' />
+      </div>
+      <div className='flex gap-6 flex-col'>
+        <div className='flex flex-col gap-[10px]'>
+          <p className='text-sm font-medium'>UTAMA</p>
+          <div className='flex gap-[10px]'></div>
+        </div>
       </div>
     </div>
   );
