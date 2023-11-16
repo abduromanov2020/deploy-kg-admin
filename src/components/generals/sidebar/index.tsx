44ee382d55817f3757e6d5bff1ce088725d4b2f3
azarnuzy <azarnuzy@gmail.com>
2023-11-16 16:31:36 +0700

    add: sidebar

--- src/components/generals/sidebar/index.tsx
+++ src/components/generals/sidebar/index.tsx
@@ -1,91 +1,210 @@
+'use client';
+
+import Link from 'next/link';
 import React, { FC, useState } from 'react';
 import { IoChevronDown } from 'react-icons/io5';
 
+import BookBookmark from '~/svg/BookBookmark.svg';
+import BookOpen from '~/svg/BookOpen.svg';
+import CalendarMonth from '~/svg/CalendarMonth.svg';
+import ChartLineUp from '~/svg/ChartLineUp.svg';
 import Dashboard from '~/svg/dashboard.svg';
 import FileCheck from '~/svg/FileCheck.svg';
-// import Users from '~/svg/users-group.svg'
-// import BookBookmark from '~/svg/BookBookmark.svg'
-// import UserFocus from '~/svg/UserFocus.svg'
 import Logo from '~/svg/Logo.svg';
+import MonitorLock from '~/svg/MonitorLock.svg';
+import MonitorPlay from '~/svg/MonitorPlay.svg';
+import OfficePlan from '~/svg/office-plan.svg';
+import SwatchBook from '~/svg/swatchbook.svg';
+import UserFocus from '~/svg/UserFocus.svg';
+import Users from '~/svg/users-group.svg';
 
 const Sidebar: FC = () => {
-  const [showDropdown, setShowDropdown] = useState(false);
+  const [openMenus, setOpenMenus] = useState<string[]>([]);
 
-  const toggleDropdown = () => {
-    setShowDropdown(!showDropdown);
+  const toggleMenu = (title: string) => {
+    setOpenMenus((prevMenus) =>
+      prevMenus.includes(title)
+        ? prevMenus.filter((item) => item !== title)
+        : [...prevMenus, title],
+    );
   };
+
   return (
     <div className='w-[280px] min-h-screen py-10 bg-sidebar'>
       <div className='pl-[35px] pb-[30px]'>
         <Logo className='w-[96px] h-full' />
       </div>
       <div className='flex gap-6 flex-col'>
-        <div className='flex flex-col gap-[10px]'>
-          <p className='text-sm font-medium text-white pl-[35px] '>UTAMA</p>
-          <div className='flex gap-[10px]  py-1 pl-[35px]'>
-            <Dashboard className='w-6 h-6' />
-            <p className='text-medium font-medium text-white'>Dashboard</p>
+        {dataSideBar.map((item, i) => (
+          <div className='flex flex-col gap-[10px]' key={i}>
+            <p className='text-sm font-medium text-white pl-[35px] '>
+              {item.title}
+            </p>
+            {item.children.map((child, index) => (
+              <div className='relative' key={index}>
+                {child.children ? (
+                  <div
+                    className='flex gap-[10px] pl-[35px] py-1 justify-between pr-[22px] cursor-pointer'
+                    onClick={() => toggleMenu(child.title)}
+                  >
+                    <div className='flex gap-[10px]'>
+                      {child.icon}
+                      <p className='text-medium font-medium text-white'>
+                        {child.title}
+                      </p>
+                    </div>
+                    <IoChevronDown
+                      className={`w-6 h-6 text-white ${
+                        openMenus.includes(child.title)
+                          ? 'transform rotate-180'
+                          : ''
+                      }`}
+                    />
+                  </div>
+                ) : (
+                  <Link
+                    href={child.link}
+                    className='flex gap-[10px]  py-1 pl-[35px]'
+                  >
+                    {child.icon}
+                    <p className='text-medium font-medium text-white'>
+                      {child.title}
+                    </p>
+                  </Link>
+                )}
+                {openMenus.includes(child.title) && child.children && (
+                  <div className='flex flex-col gap-[10px] relative w-full pl-[70px] pr-[30px] overflow-hidden transition-all duration-300'>
+                    {child.children.map((child2, index2) => (
+                      <Link
+                        href={child2.link}
+                        key={index2}
+                        className='text-medium font-medium text-white py-1'
+                      >
+                        {child2.title}
+                      </Link>
+                    ))}
+                  </div>
+                )}
+              </div>
+            ))}
           </div>
-          <div
-            className='flex gap-[10px] pl-[35px] py-1 justify-between pr-[22px] relative'
-            onClick={toggleDropdown}
-          >
-            <div className='flex gap-[10px]'>
-              <FileCheck className='w-6 h-6' />
-              <p className='text-medium font-medium text-white'>Verifikasi</p>
-            </div>
-            <IoChevronDown
-              className={`w-6 h-6 text-white ${
-                showDropdown ? 'transform rotate-180' : ''
-              }`}
-            />
-            <div
-              className={`flex flex-col gap-[10px] absolute top-10 w-full pl-[35px] pr-[30px] overflow-hidden transition-all duration-300 ${
-                showDropdown ? 'h-full' : 'h-0'
-              }`}
-            >
-              <p className='text-medium font-medium text-white py-1'>
-                Administrasi
-              </p>
-              <p className='text-medium font-medium text-white py-1'>
-                Rencana Studi
-              </p>
-            </div>
-          </div>
-          <div className='flex gap-[10px]  py-1 pl-[35px]'>
-            <Dashboard className='w-6 h-6' />
-            <p className='text-medium font-medium text-white'>Dashboard</p>
-          </div>
-          <div
-            className='flex gap-[10px] pl-[35px] py-1 justify-between pr-[22px] relative'
-            onClick={toggleDropdown}
-          >
-            <div className='flex gap-[10px]'>
-              <FileCheck className='w-6 h-6' />
-              <p className='text-medium font-medium text-white'>Verifikasi</p>
-            </div>
-            <IoChevronDown
-              className={`w-6 h-6 text-white ${
-                showDropdown ? 'transform rotate-180' : ''
-              }`}
-            />
-            <div
-              className={`flex flex-col gap-[10px] absolute top-10 w-full pl-[35px] pr-[30px] overflow-hidden transition-all duration-300 ${
-                showDropdown ? 'h-full' : 'h-0'
-              }`}
-            >
-              <p className='text-medium font-medium text-white py-1'>
-                Administrasi
-              </p>
-              <p className='text-medium font-medium text-white py-1'>
-                Rencana Studi
-              </p>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
 };
 
 export default Sidebar;
+
+const dataSideBar = [
+  {
+    title: 'UTAMA',
+    children: [
+      {
+        title: 'Beranda',
+        link: '/beranda',
+        icon: <Dashboard className='w-6 h-6' />,
+      },
+      {
+        title: 'Verifikasi',
+        icon: <FileCheck className='w-6 h-6' />,
+        children: [
+          {
+            title: 'Administrasi',
+            link: '/verifikasi/administrasi',
+          },
+          {
+            title: 'Rencana Studi',
+            link: '/verifikasi/administrasi',
+          },
+          {
+            title: 'Konversi',
+            link: '/verifikasi/konversi',
+          },
+        ],
+      },
+      {
+        title: 'User Management',
+        icon: <Users className='w-6 h-6' />,
+        children: [
+          {
+            title: 'Mahasiswa',
+            link: '/user-management/mahasiswa',
+          },
+          {
+            title: 'Dosen',
+            link: '/user-management/dosen',
+          },
+          {
+            title: 'Admin',
+            link: '/user-management/admin',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: 'UMUM',
+    children: [
+      {
+        title: 'Rencana Studi',
+        link: '/rencana-studi',
+        icon: <BookBookmark className='w-6 h-6' />,
+      },
+      {
+        title: 'Studi-ku',
+        link: '/studi-ku',
+        icon: <BookBookmark className='w-6 h-6' />,
+      },
+      {
+        title: 'Analitik Studi',
+        link: '/analitik-studi',
+        icon: <UserFocus className='w-6 h-6' />,
+      },
+      {
+        title: 'Sekilas Ilmu',
+        link: '/sekilas-ilmu',
+        icon: <BookOpen className='w-6 h-6' />,
+      },
+      {
+        title: 'Nilai & Sertifikat',
+        icon: <ChartLineUp className='w-6 h-6' />,
+        children: [
+          {
+            title: 'Nilai',
+            link: '/nilai-dan-sertifikat/nilai',
+          },
+          {
+            title: 'Sertifikat',
+            link: '/nilai-dan-sertifikat/sertifikat',
+          },
+        ],
+      },
+      {
+        title: 'Acara Kampus Gratis',
+        link: '/acara-kampus-gratis',
+        icon: <MonitorPlay className='w-6 h-6' />,
+      },
+      {
+        title: 'Penyaluran Kerja',
+        link: '/penyaluran-kerja',
+        icon: <MonitorLock className='w-6 h-6' />,
+      },
+      {
+        title: 'Perencanaan Karir',
+        link: '/perencanaan-karir',
+        icon: <SwatchBook className='w-6 h-6' />,
+      },
+      {
+        title: 'Simulasi, Drill, & Assesment',
+        link: '/simulasi-drill-dan-assesment',
+        icon: <OfficePlan className='w-6 h-6' />,
+      },
+      {
+        title: 'Kalender Mahasiswa',
+        link: '/kalender-mahasiswa',
+        icon: <CalendarMonth className='w-6 h-6' />,
+      },
+    ],
+  },
+];
