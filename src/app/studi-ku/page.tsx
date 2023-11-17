9906332ceb95d07c5a135ea55e0948eda73f9d46
FajarFE <fajarfernandi123123@gmail.com>
2023-11-17 20:55:54 +0700

    add modul,add feature filter,add according menu

---
+++ src/app/studi-ku/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+import * as React from 'react';
+
+import { MainStudiKu } from '@/modules/studi-ku';
+
+export default function StudiKu() {
+  return (
+    <main>
+      <MainStudiKu />
+    </main>
+  );
+}
