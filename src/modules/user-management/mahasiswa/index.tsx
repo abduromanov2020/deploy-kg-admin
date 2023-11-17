771a08d33f465a0e9e251950b1f397935236dcc0
imanuelcio <ciociocio009@gmail.com>
2023-11-17 12:46:59 +0700

    improvement : set up hook and request for get all user, integrated API on get mahasiswa user

--- src/modules/user-management/mahasiswa/index.tsx
+++ src/modules/user-management/mahasiswa/index.tsx
@@ -1,8 +1,12 @@
 import Link from 'next/link';
-import React from 'react';
+import { useSearchParams } from 'next/navigation';
+import React, { useState } from 'react';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { FaFileDownload, FaFilter } from 'react-icons/fa';
 
+import { useUser } from '@/hooks/user-management/getuser/hook';
+
+import { LoadingSpinner } from '@/components/LoadingSpinner';
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -23,78 +27,16 @@ import {
 } from '@/components/ui/table';
 
 const MahasiswaModule = () => {
-  const invoices = [
-    {
-      name: 'Viper',
-      paymentStatus: 'Paid',
-      faculty: 'Tata Boga',
-      id_mahasiswa: '1283798123',
-      status: 'AKTIF',
-    },
-    {
-      name: 'Iso',
-      paymentStatus: 'Pending',
-      faculty: 'Mancing Mania',
-      id_mahasiswa: '1239281390',
-      status: 'AKTIF',
-    },
-    {
-      name: 'Reyna',
-      paymentStatus: 'Unpaid',
-      faculty: 'Tahu Borax',
-      id_mahasiswa: '9128309123',
-      status: 'AKTIF',
-    },
-    {
-      name: 'Jett',
-      paymentStatus: 'Paid',
-      faculty: 'Mata mata',
-      id_mahasiswa: '91239018390',
-      status: 'AKTIF',
-    },
-    {
-      name: 'Deadlock',
-      paymentStatus: 'Paid',
-      faculty: 'Nganggur',
-      id_mahasiswa: '9128390123',
-      status: 'AKTIF',
-    },
-    {
-      name: 'Breach',
-      paymentStatus: 'Pending',
-      faculty: 'Nganggur',
-      id_mahasiswa: '9120938123',
-      status: 'CUTI',
-    },
-    {
-      name: 'Omen',
-      paymentStatus: 'Unpaid',
-      faculty: 'Teknik mancing',
-      id_mahasiswa: '912830912',
-      status: 'AKTIF',
-    },
-    {
-      name: 'Astra',
-      paymentStatus: 'Unpaid',
-      faculty: 'au dah',
-      id_mahasiswa: '91283091823',
-      status: 'TIDAK AKTIF',
-    },
-    {
-      name: 'Cyper',
-      paymentStatus: 'Unpaid',
-      faculty: 'au dah',
-      id_mahasiswa: '91283091823',
-      status: 'TIDAK AKTIF',
-    },
-    {
-      name: 'Brimstone',
-      paymentStatus: 'Unpaid',
-      faculty: 'au dah',
-      id_mahasiswa: '91283091823',
-      status: 'TIDAK AKTIF',
-    },
-  ];
+  const [searchQuery, setSearchQuery] = useState<string>('');
+  const searchParams = useSearchParams();
+  const page = searchParams.get('page') || 1;
+  const { data, isLoading, refetch } = useUser(
+    Number(page),
+    10,
+    searchQuery,
+    'STUDENT',
+  );
+
   return (
     <>
       <div className='bg-white py-10 px-6 mx-auto rounded-md'>
@@ -123,6 +65,11 @@ const MahasiswaModule = () => {
                   <DropdownMenuSeparator />
                   <DropdownMenuGroup>
                     <DropdownMenuItem>
+                      <span className='text-slate-800 bg-slate-300 px-4 rounded py-1 cursor-pointer'>
+                        All Status
+                      </span>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
                       <span className='text-green-800 bg-green-300 px-4 rounded py-1 cursor-pointer'>
                         Aktif
                       </span>
@@ -149,73 +96,65 @@ const MahasiswaModule = () => {
             </div>
           </div>
           <div className='border-2 mt-2'>
-            <Table>
-              <TableHeader>
-                <TableRow>
-                  <TableHead className='text-black font-bold'>No</TableHead>
-                  <TableHead className='text-black font-bold '>
-                    ID MAHASISWA
-                  </TableHead>
-                  <TableHead className='text-black font-bold '>
-                    NAMA MAHASISWA
-                  </TableHead>
-                  <TableHead className='text-black font-bold '>
-                    FAKULTAS
-                  </TableHead>
-                  <TableHead className='text-black font-bold '>
-                    PROGRAM STUDI
-                  </TableHead>
-                  <TableHead className='text-black font-bold text-center '>
-                    STATUS
-                  </TableHead>
-                  <TableHead className='text-black font-bold text-center  '>
-                    INFORMASI
-                  </TableHead>
-                  <TableHead className='invisible'>Edit</TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                {invoices.map((invoice, i) => (
-                  <TableRow key={i}>
-                    <TableCell className='font-medium'>{i + 1}</TableCell>
-                    <TableCell>{invoice.id_mahasiswa}</TableCell>
-                    <TableCell>{invoice.name}</TableCell>
-                    <TableCell>{invoice.faculty}</TableCell>
-                    <TableCell>{invoice.faculty}</TableCell>
-                    <TableCell>
-                      <div
-                        className={`text-center ${
-                          invoice.status == 'AKTIF'
-                            ? 'bg-green-300 text-green-800'
-                            : invoice.status == 'TIDAK AKTIF'
-                              ? 'bg-red-500 text-red-800'
-                              : 'bg-yellow-300 text-yellow-800'
-                        } font-semibold rounded-md py-1`}
-                      >
-                        {invoice.status == 'AKTIF'
-                          ? 'Aktif'
-                          : invoice.status == 'TIDAK AKTIF'
-                            ? 'Tidak Aktif'
-                            : 'Cuti'}
-                      </div>
-                    </TableCell>
-                    <TableCell className='text-center'>
-                      <Link
-                        href={`/user-management/mahasiswa/detail/${i + 1}`}
-                        className='text-blue-600 font-semibold'
-                      >
-                        Detail
-                      </Link>
-                    </TableCell>
-                    <TableCell>
-                      <button className='px-6 py-2 shadow-md text-white bg-blue-600 rounded-md  hover:bg-blue-800 hover:transition'>
-                        Edit
-                      </button>
-                    </TableCell>
+            {isLoading ? (
+              <LoadingSpinner />
+            ) : (
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead className='text-black font-bold'>No</TableHead>
+                    <TableHead className='text-black font-bold '>
+                      ID MAHASISWA
+                    </TableHead>
+                    <TableHead className='text-black font-bold '>
+                      NAMA MAHASISWA
+                    </TableHead>
+                    <TableHead className='text-black font-bold '>
+                      FAKULTAS
+                    </TableHead>
+                    <TableHead className='text-black font-bold '>
+                      PROGRAM STUDI
+                    </TableHead>
+                    <TableHead className='text-black font-bold text-center '>
+                      STATUS
+                    </TableHead>
+                    <TableHead className='text-black font-bold text-center  '>
+                      INFORMASI
+                    </TableHead>
+                    <TableHead className='invisible'>Edit</TableHead>
                   </TableRow>
-                ))}
-              </TableBody>
-            </Table>
+                </TableHeader>
+                <TableBody>
+                  {data?.data?.users.map((user, i) => (
+                    <TableRow key={i}>
+                      <TableCell className='font-medium'>{i + 1}</TableCell>
+                      <TableCell>{user.id}</TableCell>
+                      <TableCell>{user.full_name}</TableCell>
+                      <TableCell>{user.role}</TableCell>
+                      <TableCell>{user.gender}</TableCell>
+                      <TableCell>
+                        <div className='bg-green-500 text-white px-4 py-1 rounded'>
+                          Aktif
+                        </div>
+                      </TableCell>
+                      <TableCell className='text-center'>
+                        <Link
+                          href={`/user-management/mahasiswa/detail/${i + 1}`}
+                          className='text-blue-600 font-semibold'
+                        >
+                          Detail
+                        </Link>
+                      </TableCell>
+                      <TableCell>
+                        <button className='px-6 py-2 shadow-md text-white bg-blue-600 rounded-md  hover:bg-blue-800 hover:transition'>
+                          Edit
+                        </button>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            )}
           </div>
           <div className='flex justify-between place-items-center pt-5'>
             <p className='text-slate-500'>
