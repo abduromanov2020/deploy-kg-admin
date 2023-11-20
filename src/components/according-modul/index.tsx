d194d1987657ed1cc944d9a63a38f7597be33efb
azarnuzy <azarnuzy@gmail.com>
2023-11-20 14:07:18 +0700

    add: module and daftar module page

--- src/components/according-modul/index.tsx
+++ src/components/according-modul/index.tsx
@@ -26,23 +26,25 @@ export const AccordingModul: FC<AccordingModulProps> = ({
   return (
     <div className={`${className}`}>
       <Accordion type='single' collapsible>
-        <AccordionItem value='item-1' className='border-2 rounded-md '>
-          <AccordionTrigger className='data-[state=open]:text-white px-5 data-[state=open]:rounded-t-md data-[state=closed]:border-2 data-[state=closed]:rounded-md data-[state=open]:bg-primary-500'>
+        <AccordionItem value='item-1' className='border rounded-md'>
+          <AccordionTrigger className='data-[state=open]:text-white px-5 data-[state=open]:rounded-t-md  data-[state=closed]:rounded-md data-[state=open]:bg-primary-500  data-[state=open]:shadow-md no-underline'>
             {title}
           </AccordionTrigger>
-          {data.map((item, index) => {
-            return (
-              <AccordionContent
-                className='flex justify-start items-center p-2'
-                key={index}
-              >
-                <button className='w-full flex gap-2 justify-start items-center'>
-                  {item.icon}
-                  <span>{item.title}</span>
-                </button>
-              </AccordionContent>
-            );
-          })}
+          <div className='flex flex-col gap-y-3 '>
+            {data.map((item, index) => {
+              return (
+                <AccordionContent
+                  className='flex justify-start items-center p-2 px-5'
+                  key={index}
+                >
+                  <button className='w-full flex gap-2 justify-start items-center'>
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </button>
+                </AccordionContent>
+              );
+            })}
+          </div>
           <AccordionContent className='flex border-t-2 p-2 justify-end items-center '>
             <div className='w-full flex gap-5 justify-end items-center text-white '>
               <button className='bg-green-500 rounded-md px-3 py-2'>
--- src/modules/studi-ku/index.tsx
+++ src/modules/studi-ku/index.tsx
@@ -1,41 +1,97 @@
+import { Monitor } from 'lucide-react';
 import React from 'react';
-import { FaClipboard } from 'react-icons/fa';
+import { BiChevronDown } from 'react-icons/bi';
+import { BsQuestionCircle } from 'react-icons/bs';
+import { FaUserEdit } from 'react-icons/fa';
+import { IoChatboxEllipses, IoDocumentText } from 'react-icons/io5';
+import { LuClipboardList } from 'react-icons/lu';
+import { TbCloudSearch } from 'react-icons/tb';
 
 import { AccordingModul } from '@/components/according-modul';
 import { Filter } from '@/components/filter';
-
 export const MainStudiKu = () => {
   const data = [
     {
       title: 'Module',
-      icon: <FaClipboard />,
+      icon: <IoDocumentText className='text-primary-500 text-xl' />,
+      link: '/',
+    },
+    {
+      title: 'Quiz',
+      icon: <BsQuestionCircle className='text-yellow-500 text-xl' />,
+      link: '/',
+    },
+    {
+      title: 'Tugas',
+      icon: <LuClipboardList className='text-red-500 text-xl' />,
+      link: '/',
+    },
+    {
+      title: 'Diskusi',
+      icon: <IoChatboxEllipses className='text-green-500 text-xl' />,
+      link: '/',
+    },
+    {
+      title: 'Refleksi Eksplorasi',
+      icon: <TbCloudSearch className='text-orange-500 text-xl' />,
+      link: '/',
+    },
+    {
+      title: 'Live Mentoring',
+      icon: <Monitor className='text-blue-500 text-xl' />,
+      link: '/',
+    },
+    {
+      title: 'Nilai & Presensi',
+      icon: <FaUserEdit className='text-teal-500 text-xl' />,
+      link: '/',
     },
   ];
 
   const filter = [{ title: 'Pertemuan 1' }];
   return (
-    <div className='bg-white p-5 flex flex-col gap-2 w-full rounded-md shadow-md'>
-      <div className='gap-10 flex flex-row w-full'>
-        <Filter
-          icon={<FaClipboard />}
-          className='border-2 py-2 w-[180px] px-4'
-          title='Fakultas'
-          data={filter}
-        />
-        <Filter
-          icon={<FaClipboard />}
-          className='border-2 py-2 w-[180px] px-4'
-          title='Program Studi'
-          data={filter}
-        />
-        <Filter
-          className='border-2 py-2 w-[180px] px-4'
-          icon={<FaClipboard />}
-          title='Matakuliah'
-          data={filter}
-        />
+    <div className='bg-white w-full rounded-md shadow-md'>
+      <p className='text-dark-900 font-semibold  border-b border-slate-200 p-4'>
+        Studi-Ku
+      </p>
+      <div className=' p-5 flex flex-col gap-2 '>
+        <div className='gap-x-10 gap-y-3 flex flex-row w-full p-5 border border-slate-200 rounded-lg mb-5 flex-wrap'>
+          <Filter
+            icon={<BiChevronDown className='text-xl' />}
+            className='border-2 py-3 w-[180px] px-4'
+            title='Fakultas'
+            data={filter}
+          />
+
+          <Filter
+            icon={<BiChevronDown className='text-xl' />}
+            className='border-2 py-3 w-[180px] px-4'
+            title='Program Studi'
+            data={filter}
+          />
+
+          <Filter
+            icon={<BiChevronDown className='text-xl' />}
+            className='border-2 py-3 w-[180px] px-4'
+            title='Mata Kuliah'
+            data={filter}
+          />
+          <Filter
+            icon={<BiChevronDown className='text-xl' />}
+            className='border-2 py-3 w-[180px] px-4'
+            title='Kelas'
+            data={filter}
+          />
+        </div>
+        <AccordingModul title='Pertemuan 1' data={data} />
+        <AccordingModul title='Pertemuan 2' data={data} />
+        <AccordingModul title='Pertemuan 3' data={data} />
+        <AccordingModul title='Pertemuan 4' data={data} />
+        <AccordingModul title='Pertemuan 5' data={data} />
+        <AccordingModul title='Pertemuan 6' data={data} />
+        <AccordingModul title='Pertemuan 7' data={data} />
+        <AccordingModul title='Pertemuan 8' data={data} />
       </div>
-      <AccordingModul title='Pertemuan 1' data={data} />
     </div>
   );
 };
