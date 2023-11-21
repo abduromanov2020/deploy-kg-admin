8ab1972426645c19c8e6b8e812d2492ed3264b1f
azarnuzy <azarnuzy@gmail.com>
2023-11-21 16:23:36 +0700

    improvement: add dynamic validation on progress

--- src/modules/studi-ku/tambah-modul/index.tsx
+++ src/modules/studi-ku/tambah-modul/index.tsx
@@ -4,7 +4,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { convertToRaw, EditorState } from 'draft-js';
 import draftToHtml from 'draftjs-to-html';
 import dynamic from 'next/dynamic';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import toast from 'react-hot-toast';
 import { z } from 'zod';
@@ -40,9 +40,16 @@ const TambahModul = () => {
     defaultValues: {
       cover_title: '',
       cover_description: '<p></p>\n',
+      /* dynamic value */
+      /* 
+        video_description_${index}
+        video_title_${index}
+        video_link_${index}
+      */
     },
   });
 
+  /* Start Cover  */
   const [editorStateCover, setEditorStateCover] = useState<EditorState>(
     EditorState.createEmpty(),
   );
@@ -59,9 +66,35 @@ const TambahModul = () => {
       shouldDirty: true,
     });
   };
+  /* End Cover */
 
-  useEffect(() => {}, [form.formState.errors]);
+  /* Start Video Pembelajaran */
+  const [editorStateVideo, setEditorStateVideo] = useState<Array<EditorState>>([
+    EditorState.createEmpty(),
+  ]);
 
+  const [countVideo, setCountVideo] = useState<number>(1);
+
+  const handleEditorChangeVideo = (editorState: EditorState, index: number) => {
+    const newEditorState = [...editorStateVideo];
+    newEditorState[index] = editorState;
+    setEditorStateVideo(newEditorState);
+
+    const contentState = editorState.getCurrentContent();
+    const rawContentState = convertToRaw(contentState);
+    const htmlContent = draftToHtml(rawContentState);
+
+    // form.setValue('video_description_${index + 1}', htmlContent, {
+    //   shouldValidate: true,
+    //   shouldDirty: true,
+    // });
+  };
+
+  const handleAddVideo = () => {
+    setCountVideo((prev) => prev + 1);
+  };
+
+  /* End Video Pembelajaran */
   function onSubmit(data: z.infer<typeof ValidationSchemaCoverModul>) {
     console.log(data);
 
@@ -104,6 +137,35 @@ const TambahModul = () => {
                   label='Deskripsi Modul'
                   error={form.formState.errors.cover_description?.message}
                 />
+              </div>
+            </div>
+          </div>
+          <div className='px-5'>
+            <div className='flex-col flex bg-dark-200 p-5 rounded-md'>
+              <SubTitleModule title='Video Pembelajaran' />
+              <div className='flex flex-col gap-4'>
+                {Array(countVideo)
+                  .fill('')
+                  .map((_, index) => (
+                    <div
+                      className='grid grid-cols-1 lg:grid-cols-2 gap-4'
+                      key={index}
+                    >
+                      <FormField
+                        control={form.control}
+                        name='cover_title'
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel>Judul Modul</FormLabel>
+                            <FormControl>
+                              <Input placeholder='Modul 1' {...field} />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                    </div>
+                  ))}
               </div>
             </div>
           </div>
