9f165756285f504a5c452e65443c7457d4895b0f
azarnuzy <azarnuzy@gmail.com>
2023-11-22 15:43:45 +0700

    improvement: add default value in edit module page

---
+++ src/modules/studi-ku/modul/edit/constant.ts
@@ -0,0 +1,34 @@
+export const EDIT_DATA_MODULE = {
+  cover: {
+    title: 'Title Cover',
+    description: '<p>Description <strong> Cover</strong></p>',
+  },
+  video: [
+    {
+      title: 'Title Video 1',
+      link: 'https://www.youtube.com/watch?v=1q8X8t5y0ao',
+      description: '<p>Description Video <strong> 1</strong></p>',
+    },
+    {
+      title: 'Title Video 2',
+      link: 'https://www.youtube.com/watch?v=1q8X8t5y0ao',
+      description: '<p>Description Video <strong> 2</strong></p>',
+    },
+    {
+      title: 'Title Video 3',
+      link: 'https://www.youtube.com/watch?v=1q8X8t5y0ao',
+      description: '<p>Description Video <strong> 3</strong></p>',
+    },
+  ],
+  document: [
+    {
+      file: 'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf',
+    },
+    {
+      file: 'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf',
+    },
+    {
+      file: 'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf',
+    },
+  ],
+};
