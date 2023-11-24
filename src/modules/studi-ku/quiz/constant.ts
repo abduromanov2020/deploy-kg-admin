5881653cb96c76cebb41f005bc65acf60910b20e
azarnuzy <azarnuzy@gmail.com>
2023-11-24 11:01:28 +0700

    feat: quiz page

--- src/modules/studi-ku/quiz/constant.ts
+++ src/modules/studi-ku/quiz/constant.ts
@@ -8,3 +8,75 @@ export const QUIZ_MODULE_BREADCRUMBS = [
     link: '/studi-ku/quiz',
   },
 ];
+
+export const TABEL_HEAD_DETAIL_RESPONDEN_QUIZ = [
+  'NO',
+  'MAHASISWA',
+  'TANGGAL PENGERJAAN',
+  'WAKTU PENGERJAAN',
+  'BENAR',
+  'SALAH',
+  'NILAI',
+];
+
+export const DATA_TABLE_RESPONDENTS_QUIZ = {
+  max_page: 3,
+  total_data: 25,
+  data: [
+    {
+      no: '1',
+      mahasiswa: 'Rizky',
+      tanggal_pengerjaan: '20/02/2021',
+      waktu_pengerjaan: '12:00',
+      benar: '10',
+      salah: '0',
+      nilai: '100',
+    },
+    {
+      no: '2',
+      mahasiswa: 'Rizky',
+      tanggal_pengerjaan: '20/02/2021',
+      waktu_pengerjaan: '12:00',
+      benar: '10',
+      salah: '0',
+      nilai: '100',
+    },
+    {
+      no: '3',
+      mahasiswa: 'Rizky',
+      tanggal_pengerjaan: '20/02/2021',
+      waktu_pengerjaan: '12:00',
+      benar: '10',
+      salah: '0',
+      nilai: '100',
+    },
+    {
+      no: '4',
+      mahasiswa: 'Rizky',
+      tanggal_pengerjaan: '20/02/2021',
+      waktu_pengerjaan: '12:00',
+      benar: '10',
+      salah: '0',
+      nilai: '100',
+    },
+    {
+      no: '5',
+      mahasiswa: 'Rizky',
+      tanggal_pengerjaan: '20/02/2021',
+      waktu_pengerjaan: '12:00',
+      benar: '10',
+      salah: '0',
+      nilai: '100',
+    },
+    // create  20 dummy data
+    ...Array.from({ length: 20 }, (_, i) => ({
+      no: `${i + 6}`,
+      mahasiswa: `Rizky ${i + 6}`,
+      tanggal_pengerjaan: '20/02/2021',
+      waktu_pengerjaan: '12:00',
+      benar: '10',
+      salah: '0',
+      nilai: '100',
+    })),
+  ],
+};
--- src/modules/studi-ku/quiz/index.tsx
+++ src/modules/studi-ku/quiz/index.tsx
@@ -2,25 +2,50 @@
 
 import { Edit } from 'lucide-react';
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 
 import { BreadCrumb } from '@/components/BreadCrumb';
+import Pagination from '@/components/generals/pagination';
 import { Button } from '@/components/ui/button';
-import { Table, TableBody, TableCell, TableRow } from '@/components/ui/table';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
 
 import {
   SubTitleModule,
   TitleModule,
 } from '@/modules/studi-ku/modul/tambah/TitleModule';
-import { QUIZ_MODULE_BREADCRUMBS } from '@/modules/studi-ku/quiz/constant';
+import {
+  DATA_TABLE_RESPONDENTS_QUIZ,
+  QUIZ_MODULE_BREADCRUMBS,
+  TABEL_HEAD_DETAIL_RESPONDEN_QUIZ,
+} from '@/modules/studi-ku/quiz/constant';
 
 const QuizModule = () => {
+  const table_respondents_head = TABEL_HEAD_DETAIL_RESPONDEN_QUIZ;
+  const data = DATA_TABLE_RESPONDENTS_QUIZ;
+
+  const [page, setPage] = useState(1);
+
+  const handlePageChange = (page: number) => {
+    setPage(page);
+  };
+
+  const filteredData = data.data.filter((item, index) => {
+    return index >= (Number(page) - 1) * 10 && index < Number(page) * 10;
+  });
+
   return (
     <div className='flex flex-col gap-6'>
       <div className='bg-white w-full rounded-md shadow-md p-5'>
         <BreadCrumb items={QUIZ_MODULE_BREADCRUMBS} className='!p-0 ' />
       </div>
-      <div className='bg-white flex flex-col gap-5 rounded-md pb-5 '>
+      <div className='bg-white flex flex-col gap-8 rounded-md pb-5 '>
         <div className='flex justify-between items-center'>
           <TitleModule title='Quiz Mata Kuliah Manajemen Keuangan' />
         </div>
@@ -78,6 +103,46 @@ const QuizModule = () => {
               </TableRow>
             </TableBody>
           </Table>
+        </div>
+        <div className='flex-col flex px-5 gap-2'>
+          <SubTitleModule title='Detail Responden Quiz' />
+          <Table className='border-2'>
+            <TableHeader>
+              <TableRow>
+                {table_respondents_head.map((item, index) => (
+                  <TableHead key={index} className='text-black font-bold'>
+                    {item}
+                  </TableHead>
+                ))}
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {filteredData.map((item, index) => (
+                <TableRow key={index}>
+                  <TableCell className='font-medium'>
+                    {index + 1 + (Number(page) - 1) * 10}
+                  </TableCell>
+                  <TableCell>{item.mahasiswa}</TableCell>
+                  <TableCell>{item.tanggal_pengerjaan}</TableCell>
+                  <TableCell>{item.waktu_pengerjaan ?? '-'}</TableCell>
+                  <TableCell>{item.benar ?? '-'}</TableCell>
+                  <TableCell>{item.salah ?? '-'}</TableCell>
+                  <TableCell>{item.nilai ?? '-'}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+          <div className='flex justify-between place-items-center pt-5'>
+            <p className='text-slate-500'>
+              Menampilkan {page} hingga {Number(page) * 10} dari{' '}
+              {data?.total_data} entri
+            </p>
+            <Pagination
+              currentPage={Number(page)}
+              totalPages={Number(data?.max_page)}
+              onPageChange={handlePageChange}
+            />
+          </div>
         </div>
       </div>
     </div>
