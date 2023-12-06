7d9330faade15995c170e00e8cfc6a0cb823d110
azarnuzy <azarnuzy@gmail.com>
2023-12-06 17:36:43 +0700

    improvemetn: add type form and validation schema for add pendahuluan page

---
+++ src/app/studi-ku/pendahuluan/edit/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import PendahuluanEditModule from '@/modules/studi-ku/pendahuluan/edit';
+
+const EditPendahuluanPage = () => {
+  return (
+    <div>
+      <PendahuluanEditModule />
+    </div>
+  );
+};
+
+export default EditPendahuluanPage;
---
+++ src/lib/validation/studi-ku/pendahuluan.ts
@@ -0,0 +1,34 @@
+import { z } from 'zod';
+const MAX_FILE_SIZE = 2000000;
+const ACCEPTED_PDF_TYPES = ['application/pdf'];
+
+export const validationSchemaPendahuluan = z.object({
+  title: z
+    .string({
+      required_error: 'Judul pendahuluan harus diisi.',
+    })
+    .min(1, { message: 'Judul pendahuluan harus diisi.' }),
+  description: z
+    .string({
+      required_error: 'Deskripsi pendahuluan harus diisi.',
+    })
+    .min(1, { message: 'Deskripsi pendahuluan harus diisi.' }),
+  video: z
+    .string({
+      required_error: 'Video pendahuluan harus diisi.',
+    })
+    .min(1, { message: 'Video pendahuluan harus diisi.' }),
+  document: z
+    .any()
+    .refine(
+      (files: File[]) => files !== undefined && files?.length >= 1,
+      'Harus ada file yang di upload.',
+    )
+    .refine((files: File[]) => {
+      return files !== undefined && files?.[0]?.size <= MAX_FILE_SIZE;
+    }, 'Ukuran maksimun adalah 2mb.')
+    .refine(
+      (files: File[]) => ACCEPTED_PDF_TYPES.includes(files?.[0]?.type),
+      'hanya menerima .pdf.',
+    ),
+});
