747133d6cdb36a5db371c0a3b40403ca0883a8c3
azarnuzy <azarnuzy@gmail.com>
2023-12-20 17:50:01 +0700

    improvement: add constant prensensi dan nilai data

--- src/modules/studi-ku/presensi-dan-nilai/index.tsx
+++ src/modules/studi-ku/presensi-dan-nilai/index.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
+import { BreadCrumb } from '@/components/BreadCrumb';
+
+import { TitleModule } from '@/modules/studi-ku/modul/tambah/TitleModule';
+import { PRESENSI_DAN_NILAI_BREADCRUMBS } from '@/modules/studi-ku/presensi-dan-nilai/constant';
+
 const PresensiDanNilaiModule = () => {
-  return <div>PresensiDanNilaiModule</div>;
+  return (
+    <div className='flex flex-col gap-6'>
+      <div className='bg-white w-full rounded-md shadow-md p-5'>
+        <BreadCrumb items={PRESENSI_DAN_NILAI_BREADCRUMBS} className='!p-0 ' />
+      </div>
+      <div className='bg-white flex flex-col gap-8 rounded-md pb-5 '>
+        <div className='flex justify-between items-center'>
+          <TitleModule title='Presensi: Pertemuan 1' />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PresensiDanNilaiModule;
