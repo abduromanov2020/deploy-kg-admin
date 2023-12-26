4cf74b83e53993b5e58fdc9ffef2276df2ed11ff
imanuelcio <ciociocio009@gmail.com>
2023-12-26 18:44:48 +0700

    improvement:set up request api for edi admin user, fix zod validate and form data

--- src/lib/validation/user-management/index.ts
+++ src/lib/validation/user-management/index.ts
@@ -72,25 +72,11 @@ export const EditAdminUserValidationSchema = z.object({
       required_error: 'Email Dibutuhkan',
     })
     .min(1, { message: 'Email Dibutuhkan' }),
+  password: z.string().min(1, { message: 'Password harus diisi' }),
 
-  status: z
+  role: z
     .string({
       required_error: 'Program Studi Dibutuhkan',
     })
     .min(1, { message: 'Program Studi Dibutuhkan' }),
-  self_foto: z
-    .any()
-    .refine(
-      (files: File[]) => files !== undefined && files?.length >= 1,
-      'Harus ada file yang di upload.',
-    )
-    .refine(
-      (files: File[]) =>
-        files !== undefined && files?.[0]?.size <= MAX_FILE_SIZE,
-      'Ukuran maksimun adalah 3mb.',
-    )
-    .refine(
-      (files: File[]) => ACCEPTED_IMAGE_TYPES.includes(files?.[0].type),
-      'hanya menerima .jpg, .jpeg, dan .webp.',
-    ),
 });
